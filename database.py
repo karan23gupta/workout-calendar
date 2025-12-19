804a1c9ac943ce43e71a4afe5011498462097c64
Karan Gupta <karangupta12527@gmail.com>
2025-12-19 12:22:35 +0530

    import fixes

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
-from sqlalchemy import create_engine
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
